232f291fb0d04e3e572c4f37e9e014114415e1f4
Castro-Erick <150728304+Castro-Erick@users.noreply.github.com>
2024-03-29 14:30:47 -0700

    Message Purging Update
    Added a function to allow for automatic deletion of channel messages on a set interval.

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,9 +1,12 @@
 import asyncio
 import discord
+import json
 import logging
+import os
 import youtube_dl
 
 from discord.ext import commands
+from discord.ext import tasks
 from discord import FFmpegPCMAudio
 from random import randint
 
@@ -19,6 +22,9 @@ intents.voice_states = True
 prefix = '!'
 client = commands.Bot(command_prefix=prefix, intents=intents)
 
+DEFAULT_PURGE_INTERVAL = 3600 #1 hour in seconds
+PURGE_INTERVAL_FILE = 'purge_interval.json'
+PURGE_LIST_FILE = 'purge_list.json'
 yt_queue = []
 yt_queue_title_names = []
 
@@ -43,6 +49,26 @@ def download_audio(url):
     
     return audio_url
 
+#loads "purge_interval.json"
+def load_purge_interval():
+    if not os.path.exists(PURGE_INTERVAL_FILE):
+        return DEFAULT_PURGE_INTERVAL
+    try:
+        with open(PURGE_INTERVAL_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return DEFAULT_PURGE_INTERVAL
+
+#loads "purge_list.json"
+def load_purge_list():
+    if not os.path.exists(PURGE_LIST_FILE):
+        return []
+    try:
+        with open(PURGE_LIST_FILE, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
 #returns random meatwad clip
 def meatwad():
     meatwad_clips = ["Local Directories Here", "For Sound Clips", "Originally tested with meatwad clips"]
@@ -59,11 +85,49 @@ def play_next(ctx) -> None:
     audio_source = FFmpegPCMAudio(url)
     ctx.voice_client.play(audio_source, after=lambda e: play_next(ctx))
 
+#saves "purge_interval.json"
+def save_purge_interval(purge_interval):
+    with open(PURGE_INTERVAL_FILE, 'w') as file:
+        json.dump(purge_interval, file)
+
+#saves "purge_list.json"
+def save_purge_list(purge_list):
+    with open(PURGE_LIST_FILE, 'w') as file:
+        json.dump(purge_list, file)
+
+
+
+#loop to purge channels of messages
+@tasks.loop(seconds=purge_interval)
+async def purge_messages():
+    for channel_id in purge_list:
+        channel = client.get_channel(channel_id)
+        if channel:
+            await channel.purge()
+
+
+
+#loads purge interval on bot startup
+purge_interval = load_purge_interval()
+
+#loads purge list on bot startup
+purge_list = load_purge_list()
+
 #displays message on launch when bot is ready
 @client.event
 async def on_ready():
     print("MeatBot reporting for duty!")
     print("---------------------")
+    purge_messages.start()
+
+
+
+@tasks.loop(seconds=purge_interval)
+async def purge_messages():
+    for channel_id in purge_list:
+        channel = client.get_channel(channel_id)
+        if channel:
+            await channel.purge()
 
 #Displays current song
 @client.command()
@@ -77,8 +141,7 @@ async def cyber_news(ctx):
     
     urls_txt = '\n'.join(cyber_url_list)
     
-    await ctx.send(f'Here\'s a list of websites related to Cyber News:\n{urls_txt}')
-        
+    await ctx.send(f'Here\'s a list of websites related to Cyber News:\n{urls_txt}')        
 
 #flips a coin
 @client.command()
@@ -87,6 +150,17 @@ async def flip(ctx):
         await ctx.send('heads')
     else:
         await ctx.send('tails')
+
+#changes the frequency at which the channels on the purge list are purged
+@client.command()
+async def interval(ctx, new_interval: int = None):
+    global purge_interval
+    if new_interval is None:
+        await ctx.send(f"Affected channels will be purged every {purge_interval} seconds.")
+    else:
+        purge_interval = new_interval
+        save_purge_interval(purge_interval)
+        await ctx.send(f"Default purge interval has been set to {new_interval}")
 
 #disconnects bot from channel (always good to keep, maybe have automatic timeout eventually)
 @client.command(pass_context = True)
@@ -152,6 +226,31 @@ async def play(ctx, yt_url=None):
         print(f"Error: {e}")
         await ctx.send("Please check to make sure url was input correctly.")
 
+@client.command()
+async def purgelist(ctx, arg: str, channel_name: str):
+
+    #checks to see if channel exists
+    channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+    if channel is None:
+        await ctx.send(f"channel '{channel_name}' does not exist.")
+        return
+
+    #adds or removes channel from the purge list
+    if arg == "add":
+        if channel.id not in purge_list:
+            purge_list.append(channel.id)
+            save_purge_list(purge_list)
+            await ctx.send(f"Channel '{channel_name}' has been added to the purge list.")
+        else: 
+            await ctx.send(f"Channel '{channel_name}' is already on the purge list.")
+    elif arg == "remove":
+        if channel.id in purge_list:
+            purge_list.remove(channel.id)
+            save_purge_list(purge_list)
+            await ctx.send(f"Channel '{channel_name}' has been removed from purge list.")
+        else:
+            await ctx.send(f"Channel '{channel_name}' is not on the purge list.")
+
 #checks queued up songs
 @client.command()
 async def queue(ctx):
@@ -163,11 +262,11 @@ async def queue(ctx):
 
 #die roll (change to format xdy where x is the number of y sided dice)
 @client.command()
-async def roll(ctx, arg):
+async def roll(ctx, arg: int):
     try:
         result = randint(1, int(arg))
         await ctx.send(result)
-    except:
+    except ValueError:
         await ctx.send(f'\"{arg}\" isn\'t a number')
 
 #skips current song
